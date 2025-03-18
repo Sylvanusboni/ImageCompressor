0502cdef6aa970a327704620e6ff0bd56fa10199
Sylvanusboni <114754208+Sylvanusboni@users.noreply.github.com>
2025-03-18 19:10:30 +0100

    Update Error.hs

--- app/Error.hs
+++ app/Error.hs
@@ -17,11 +17,30 @@ errors = putStrLn "USAGE: ./imageCompressor -n N -l L -f F\n\n\
     \tF\tpath to the file containing the colors of the pixels"
     >> exitWith (ExitFailure(84))
 
+errorsCase :: IO ()
+errorsCase = putStrLn "    ██████╗  ██████╗ ███╗   ██╗██╗     █████╗ ██████╗ ████████╗\n\
+\    ██╔══██╗██╔═══██╗████╗  ██║██║    ██╔══██╗██╔══██╗╚══██╔══╝\n\
+\    ██████╔╝██║   ██║██╔██╗ ██║██║    ███████║██████╔╝   ██║   \n\
+\    ██╔══██╗██║   ██║██║╚██╗██║██║    ██╔══██║██╔══██╗   ██║   \n\
+\    ██████╔╝╚██████╔╝██║ ╚████║██║    ██║  ██║██║  ██║   ██║   \n\
+\    ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \n\
+    \USAGE: ./imageCompressor -n N -l L -f F [-opt]\n\n\
+    \\tN\tnumber of colors in the final image\n\
+    \\tL\tconvergence limit\n\
+    \\tF\tpath to the file (Image or Txt)\n\
+    \\t[opt]: execution options (img or txt) [txt] by default\n\
+    \For option img, only (png, jpeg, bitmap, gif, tga, tiff and radiance) formats are accepted\n\
+    \In image option check the output Result in result.png"
+    >> exitWith (ExitFailure(84))
+
 isNum :: String -> Bool
 isNum [] = False
 isNum a = if all isDigit a
         then True
         else False
+
+validOption :: String -> Bool
+validOption str = if (str == "img" || str == "txt") then True else False
 
 isDouble :: String -> Int -> Bool -> Bool
 isDouble [] _ _ = True
@@ -40,4 +59,7 @@ checkArgs ("-f":f:ls) = do
     if (True)
         then checkArgs ls
         else return False
+checkArgs ("-opt":option:ls) = if (validOption option)
+    then checkArgs ls
+    else return False
 checkArgs (_:ls) = return False
